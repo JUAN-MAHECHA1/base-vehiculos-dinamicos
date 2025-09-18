2d4d6a0554ea68b9d478c55e9eec03943bb2426c
Juan Mahecha <mahechajuanda29@gmail.com>
2025-09-18 17:39:27 -0500

    Creamos las constantes globales para los elementos principales

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const input = document.getElementsByClassName('form-control');
+const label = document.getElementsByClassName('form-label');
+const vehiculoForm = document.getElementById('vehiculo-form');
+const itemVehiculo = document.getElementsByClassName('item-vehiculo');
+
+function createVehiculoForm(textoVehiculo){
+    const vehiculo = document.createElement('div');
+    
+}
